e5b03220dbe7f438efcd6199555602abee22fe26
ppawelo1 <pawel.petelewicz@op.pl>
2018-04-13 12:09:02 +0200

    komentarze

--- js/app_jq.js
+++ js/app_jq.js
@@ -10,8 +10,8 @@
     const ch = canvas.height;
 
     const ballSize = 20; //wielkosć piłki
-    let ballX = cw / 2 - ballSize / 2
-    let ballY = ch / 2 - ballSize / 2
+    let ballX = cw / 2 - ballSize / 2;  //wielkosć piłki x
+    let ballY = ch / 2 - ballSize / 2;  //wielkosć piłki y
 
     const paddelHeight = 100; //wysokość rakietki gracza
     const paddelWidth = 20; //szerokość rakietki gracza
@@ -46,7 +46,7 @@
       ctx.fillRect(ballX, ballY, ballSize, ballSize);
 
 
-      ballX += ballSpeedX;
+      ballX += ballSpeedX;   ///koreslanie szybkości piłki
       ballY += ballSpeedY;
 
       if (ballY <= 0 || ballY + ballSize >= ch ) {
@@ -71,7 +71,7 @@
 
     function table() {
 
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'black';  //stól do grania
       ctx.fillRect(0, 0, cw, ch);
 
       for (let linePosition = 20; linePosition < ch; linePosition += 30) {
@@ -106,20 +106,20 @@
              ballSpeedX += .3;
         }
          else  if (ballSpeedY > 0 && ballSpeedY > -16) {
-              ballSpeedY -= .3;
+              ballSpeedY -= .3
          }
      }
 
      function aiPosition() { //Sztuczna inteligencja paletki
       const middlePaddel= aiY + paddelHeight / 2 ;
       const middleBall = ballY + ballSize / 2;
-      
+
       if (ballX > 500) {
-        
+
         if (middlePaddel - middleBall > 200) {
- 
+
           aiY -= 24;
-          
+
         } else if (middlePaddel - middleBall > 50) {
           aiY -= 10;
         }
@@ -129,33 +129,32 @@
           aiY += 10;
         }
        }
-       
+
       if (ballX <= 500 && ballX > 100) {
         if (middlePaddel - middleBall > 100) {
           aiY -= 3;
-        } 
+        }
  if (middlePaddel - middleBall < -100) {
           aiY += 3;
         }
       }
       if (aiY >= ch - paddelHeight) {
-        aiY = ch - paddelHeight
+        aiY = ch - paddelHeight;
       }
-	
+
       if (aiY <= 0) {
         aiY = 0;
       }
     }
 
-    canvas.addEventListener("mousemove", playerPosition)
+    canvas.addEventListener("mousemove", playerPosition);
 
     function game() {
-      table()
-      ball()
-      player()
-      ai()
-      aiPosition()
+      table();
+      ball();
+      player();
+      ai();
+      aiPosition();
     }
 
-    setInterval(game, 1000 / 60)
-
+    setInterval(game, 1000 / 60);
